563a73697c330ef922cbbb00c0d6555d8a65226e
Joket_abc <15005771700@qq.com>
2021-03-29 17:11:23 +0800

    机器人判断优化 增加某些消息自动回复

--- KioCG-BotExtend/src/com/kiocg/BotExtend/listeners/BotMessage.java
+++ KioCG-BotExtend/src/com/kiocg/BotExtend/listeners/BotMessage.java
@@ -1,6 +1,7 @@
 package com.kiocg.BotExtend.listeners;
 
 import com.kiocg.BotExtend.message.CommandsPublic;
+import com.kiocg.BotExtend.message.MessagesRemind;
 import com.kiocg.BotExtend.utils.Utils;
 import com.kiocg.qqBot.events.message.AsyncFriendMessageEvent;
 import com.kiocg.qqBot.events.message.AsyncGroupMessageEvent;
@@ -14,7 +15,12 @@ public class BotMessage implements @NotNull Listener {
     public void onFriendMessage(final @NotNull AsyncFriendMessageEvent event) {
         final net.mamoe.mirai.event.events.FriendMessageEvent e = event.getEvent();
 
-        final String userCommand = Utils.getUserCommand(e.getMessage().contentToString().trim());
+        final String msg = e.getMessage().contentToString().trim();
+        if (msg.length() > 100) {
+            return;
+        }
+
+        final String userCommand = Utils.getUserCommand(msg);
         if (userCommand != null && !userCommand.isEmpty()) {
             new CommandsPublic().onCommandsPublic(e.getSender(), e.getSender(), userCommand);
         }
@@ -24,9 +30,16 @@ public class BotMessage implements @NotNull Listener {
     public void onGroupMessage(final @NotNull AsyncGroupMessageEvent event) {
         final net.mamoe.mirai.event.events.GroupMessageEvent e = event.getEvent();
 
-        final String userCommand = Utils.getUserCommand(e.getMessage().contentToString().trim());
+        final String msg = e.getMessage().contentToString().trim();
+        if (msg.length() > 100) {
+            return;
+        }
+
+        final String userCommand = Utils.getUserCommand(msg);
         if (userCommand != null && !userCommand.isEmpty()) {
             new CommandsPublic().onCommandsPublic(e.getGroup(), e.getSender(), userCommand);
+        } else {
+            new MessagesRemind().onMessages(e.getGroup(), msg.toLowerCase());
         }
     }
 
@@ -34,7 +47,12 @@ public class BotMessage implements @NotNull Listener {
     public void onGroupTempMessage(final @NotNull AsyncGroupTempMessageEvent event) {
         final net.mamoe.mirai.event.events.GroupTempMessageEvent e = event.getEvent();
 
-        final String userCommand = Utils.getUserCommand(e.getMessage().contentToString().trim());
+        final String msg = e.getMessage().contentToString().trim();
+        if (msg.length() > 100) {
+            return;
+        }
+
+        final String userCommand = Utils.getUserCommand(msg);
         if (userCommand != null && !userCommand.isEmpty()) {
             new CommandsPublic().onCommandsPublic(e.getSender(), e.getSender(), userCommand);
         }
---
+++ KioCG-BotExtend/src/com/kiocg/BotExtend/message/MessagesRemind.java
@@ -0,0 +1,20 @@
+package com.kiocg.BotExtend.message;
+
+import net.mamoe.mirai.contact.Group;
+import org.bukkit.event.EventHandler;
+import org.jetbrains.annotations.NotNull;
+
+public class MessagesRemind {
+    @EventHandler
+    public void onMessages(final @NotNull Group group, final @NotNull String msg) {
+        if (msg.contains("按键精灵") || msg.contains("鼠标宏") || msg.contains("自动钓鱼")) {
+            group.sendMessage("你不能使用诸如按键精灵、鼠标宏、自动钓鱼等自动操作程序，这将会被视为作弊。");
+        } else if (msg.contains("刷沙子") || msg.contains("刷tnt") || msg.contains("刷地毯") || msg.contains("卡基岩") || msg.contains("地狱顶")) {
+            group.sendMessage("你不能利用BUG刷物品、刷数据、卡基岩、卡地狱(下届)顶等。");
+        } else if (msg.contains("种子") && (msg.contains("服务器") || msg.contains("世界") || msg.contains("地图"))) {
+            group.sendMessage("你不能向管理员索要物品、指令操作、世界种子等，所有管理员同玩家均没有任何特权。");
+        } else if (msg.contains("村民") && (msg.contains("僵尸")) || (msg.contains("海龟蛋") && (msg.contains("僵尸") || msg.contains("猪人") || msg.contains("猪灵")))) {
+            group.sendMessage("出于优化需要，所有实体的AI行为均与原版有所不同，有关实体的反馈将不受支持。");
+        }
+    }
+}
