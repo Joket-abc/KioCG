2ca1292380dc76a56b1c60a5c09eccb77cd5fbea
Joket_abc <15005771700@qq.com>
2021-06-05 03:31:22 +0800

    机器人消息更新

--- KioCG-BotExtend/src/com/kiocg/BotExtend/message/MessagesRemind.java
+++ KioCG-BotExtend/src/com/kiocg/BotExtend/message/MessagesRemind.java
@@ -25,13 +25,13 @@ public class MessagesRemind {
         }
 
         if (msg.contains("按键精灵") || msg.contains("鼠标宏") || msg.contains("键盘宏") || msg.contains("连点") || msg.contains("自动钓鱼")) {
-            group.sendMessage("你不能使用诸如按键精灵、宏操作、自动钓鱼等自动操作程序，这将会被视为作弊。");
+            group.sendMessage("你不能使用如按键精灵、鼠标宏、连点器、自动钓鱼等自动操作程序，会被视为作弊。");
         } else if (((msg.contains("刷") || msg.contains("复制")) && (msg.contains("沙子") || msg.contains("tnt") || msg.contains("地毯") || msg.contains("铁轨"))) || msg.contains("卡基岩") || msg.contains("地狱顶")) {
-            group.sendMessage("你不能利用BUG刷物品、刷数据、卡基岩、卡地狱(下届)顶等。");
+            group.sendMessage("你不能利用BUG刷物品、刷数据、卡基岩、卡地狱(下届)顶等，发现BUG应向群主反馈。");
         } else if (msg.contains("种子") && (msg.contains("服务器") || msg.contains("世界") || msg.contains("地图"))) {
-            group.sendMessage("你不能向管理员索要物品、指令操作、世界种子等，所有管理员同玩家均没有任何特权。");
+            group.sendMessage("你不能向管理员索要物品、权限指令、世界种子，所有管理员同玩家均没有任何特权。");
         } else if ((msg.contains("村民") && !msg.contains("僵尸村民") && msg.contains("僵尸")) || (msg.contains("海龟蛋") && (msg.contains("僵尸") || msg.contains("猪人") || msg.contains("猪灵")))) {
-            group.sendMessage("出于优化需要，所有实体的AI行为均与原版有所不同，有关实体的反馈将不受支持。");
+            group.sendMessage("出于优化的需要，大部分实体的AI行为与原版有所不同，有关实体的反馈将不受支持。");
         }
 
         replyTime.put(groupID, System.currentTimeMillis());
