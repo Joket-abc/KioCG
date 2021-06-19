26e0528a2a36f1364fec955fc4b3c716dd08df02
Joket_abc <15005771700@qq.com>
2021-06-19 19:34:01 +0800

    放置复制告示牌

--- KioCG-LittleThings/src/com/kiocg/LittleThings/listeners/Misc/copySign.java
+++ KioCG-LittleThings/src/com/kiocg/LittleThings/listeners/Misc/copySign.java
@@ -1,15 +1,19 @@
 package com.kiocg.LittleThings.listeners.Misc;
 
 import com.destroystokyo.paper.MaterialTags;
+import net.kyori.adventure.text.Component;
 import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BlockStateMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -34,7 +38,7 @@ public class copySign implements Listener {
             if (!MaterialTags.SIGNS.isTagged(Objects.requireNonNull(itemStack))) {
                 return;
             }
-        } catch (final NullPointerException ignore) {
+        } catch (final @NotNull NullPointerException ignore) {
             return;
         }
 
@@ -43,13 +47,32 @@ public class copySign implements Listener {
             itemStack.setAmount(itemStack.getAmount() - 1);
 
             final ItemStack signStack = itemStack.clone().asOne();
+            final ItemMeta itemMeta = itemStack.getItemMeta();
+            itemMeta.displayName(Component.text(Objects.requireNonNull(itemStack.getI18NDisplayName())).append(Component.text("(拷贝)")));
 
-            final BlockStateMeta blockStateMeta = (BlockStateMeta) signStack.getItemMeta();
+            final BlockStateMeta blockStateMeta = (BlockStateMeta) itemMeta;
             Objects.requireNonNull(blockStateMeta).setBlockState(block.getState());
             signStack.setItemMeta(blockStateMeta);
 
             inventory.addItem(signStack);
             e.setCancelled(true);
         }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onBlockPlace(final @NotNull BlockPlaceEvent e) {
+        final ItemStack itemStack = e.getItemInHand();
+
+        if (!MaterialTags.SIGNS.isTagged(Objects.requireNonNull(itemStack)) && !itemStack.displayName().toString().endsWith("(拷贝)")) {
+            return;
+        }
+
+        final Sign signBlock = (Sign) e.getBlockPlaced().getState();
+        final Sign signItem = (Sign) ((BlockStateMeta) itemStack.getItemMeta()).getBlockState();
+
+        signBlock.line(1, signItem.line(1));
+        signBlock.line(2, signItem.line(2));
+        signBlock.line(3, signItem.line(3));
+        signBlock.line(4, signItem.line(4));
     }
 }
