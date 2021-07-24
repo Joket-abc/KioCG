f5190148ffc7a9f835b8e9ad014a9c4ac44c7c7b
Joket_abc <15005771700@qq.com>
2021-07-25 07:01:10 +0800

    AntiAutoFish

--- KioCG-AntiAutoFish/src/com/kiocg/AntiAutoFish/Utils.java
+++ KioCG-AntiAutoFish/src/com/kiocg/AntiAutoFish/Utils.java
@@ -9,5 +9,5 @@ public class Utils {
     // 存储玩家、鱼上钩时间戳
     public static final Map<Player, Long> playerBiteTime = new HashMap<>();
     // 存储玩家、疑似自动钓鱼次数
-    public static final Map<Player, Integer> playerAutoCount = new HashMap<>();
+    public static final Map<Player, Integer> playerAutoFishCount = new HashMap<>();
 }
